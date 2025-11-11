610d7ce9e6d53f001a4258cfe93eecbc85755eb8
aleksman-git <smolinav1987@yandex.ru>
2025-11-11 23:11:21 +0300

    alter models add tuple

--- incident_app/models.py
+++ incident_app/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 
 class Incident(models.Model):
+    status_new = 'new'
+    status_in_progress = 'in_progress'
+    status_resolved = 'resolved'
+    status_closed = 'closed'
+
+    status_choices = [
+        (status_new, 'Новый'),
+        (status_in_progress, 'В работе'),
+        (status_resolved, 'Решен'),
+        (status_closed, 'Закрыт'),
+    ]
 
     incident_id = models.BigIntegerField(verbose_name='ID инцидента', primary_key=True)
     description = models.TextField(max_length=200, verbose_name='Описание')
-    status = models.CharField(max_length=20, verbose_name='Статус')
+    status = models.CharField(
+        max_length=20,
+        choices=status_choices,
+        default=status_new,
+        verbose_name='Статус')
     source = models.CharField(max_length=20, verbose_name='Источник')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
